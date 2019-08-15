9eeb5f15d122326dbd230ba3e2bf5ec1b25b6c60
Sarah Hoegen <sarihosan@gmail.com>
2019-08-14 22:37:27 -0300

    Add files via upload

---
+++ listas/exeGeraSite.py
@@ -0,0 +1,16 @@
+titulo= input("Inserir titutlo")
+
+cabecalho=input("Inserir cabecalho")
+
+titulopost=input("Insira o titulo da psotagem")
+
+link = input("Insira o link")
+
+descricao = input("insira a descrição")
+
+
+
+arquivo = open('site.html','a')
+arquivo.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>'''+titulo'''</title></head><body><h1>'''+cabecalho'''</h1><br><h2>'''+titulopost'''<a href=" '''+link''' "></a><br><p>'''+descricao'''</p></body></html> ''' )
+
+arquivo.close()
