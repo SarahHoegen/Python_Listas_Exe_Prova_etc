e3274c89e3c3fe3cc09715218356ce86ad328774
Sarah Hoegen <sarihosan@gmail.com>
2019-08-15 11:31:35 -0300

    Update exeGeraSite.py

--- listas/exeGeraSite.py
+++ listas/exeGeraSite.py
@@ -13,6 +13,6 @@ descricao = input("insira a descrição")
 
 
 arquivo = open('site.html','w')
-arquivo.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>'''+titulo+'''</title></head><body><h1>'''+cabecalho+'''</h1><br><h2>'''+titulopost+'''</h2><a href=" '''+link+''' ">'''+nomelink+'''</a><p>'''+descricao+'''</p></body></html> ''' )
+arquivo.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>'''+titulo+'''</title></head><body><h1>'''+cabecalho+'''</h1><br><h2>'''+titulopost+'''</h2><a href=" '''+link+''' ">'''+nomelink+'''</a><p>'''+descricao+'''</p><a href=" '''+link2+''' ">'''+nomelink2+'''</a><p>'''+descricao2+'''</p></body></html> ''' )
 
 arquivo.close()
