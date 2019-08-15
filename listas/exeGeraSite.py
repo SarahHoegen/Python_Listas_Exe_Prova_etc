466104952f17e3b6a519908bf0ce4a86a2800bcd
Sarah Hoegen <sarihosan@gmail.com>
2019-08-15 10:53:18 -0300

    Update exeGeraSite.py

--- listas/exeGeraSite.py
+++ listas/exeGeraSite.py
@@ -6,11 +6,13 @@ titulopost=input("Insira o titulo da psotagem")
 
 link = input("Insira o link")
 
+nomelink = input("Insira o nome do  link")
+
 descricao = input("insira a descrição")
 
 
 
 arquivo = open('site.html','w')
-arquivo.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>'''+titulo+'''</title></head><body><h1>'''+cabecalho+'''</h1><br><h2>'''+titulopost+'''</h2><a href=" '''+link+''' ">'''+descricao+'''</a></body></html> ''' )
+arquivo.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>'''+titulo+'''</title></head><body><h1>'''+cabecalho+'''</h1><br><h2>'''+titulopost+'''</h2><a href=" '''+link+''' ">'''+nomelink+'''</a><p>'''+descricao+'''</p></body></html> ''' )
 
 arquivo.close()
